d8d518083631f986f8b4857c64bf4990d06e6a28
Valeria225 <130021508+Valeria225@users.noreply.github.com>
2024-04-29 02:15:51 -0400

    Update update_employee.php

--- update_employee.php
+++ update_employee.php
@@ -10,10 +10,10 @@ if(isset($_GET['employee_name']) && isset($_GET['new_name'])){
     $new_name = $_GET['new_name'];
 
     // Prepare SQL and bind parameters
-    $sql = "UPDATE Employee SET employee_name = $new_name WHERE employee_id = $employee_name";
+    $sql = "UPDATE Employee SET name = ? WHERE name = ?";
 
     if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param("si", $new_name, $employee_id);
+        $stmt->bind_param("ss", $new_name, $employee_name);
         if ($stmt->execute()) {
             echo "Employee updated successfully";
             header("Location: restaurant.php"); // Redirect back to the main customer page
